baf4705b869d3f6ae3ee45467241e54c38ffa36d
Thiago Mendes <thiagomendes_95@hotmail.com>
2020-04-27 00:16:35 -0300

    Implementação Método Listar

--- src/main/java/br/com/ordemservico/rest/thiagomds/api/controller/OrdemServicoController.java
+++ src/main/java/br/com/ordemservico/rest/thiagomds/api/controller/OrdemServicoController.java
@@ -1,9 +1,12 @@
 package br.com.ordemservico.rest.thiagomds.api.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,19 +14,32 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.ordemservico.rest.thiagomds.domain.model.OrdemServico;
+import br.com.ordemservico.rest.thiagomds.domain.repository.OrdemServicoRepository;
 import br.com.ordemservico.rest.thiagomds.domain.service.GestaoOrdemServicoService;
 
 @RestController
 @RequestMapping("/ordens-servico")
 public class OrdemServicoController {
 
+	// REPOSTORIOS
 	@Autowired
 	private GestaoOrdemServicoService gestaoOrdemServico;
 	
+	@Autowired
+	private OrdemServicoRepository ordemServicoRespository;
+	
+	
+	// CADASTRAR ORDEM DE SERVIÇO
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public OrdemServico criar( @Valid @RequestBody OrdemServico ordemServico ) {
 		return gestaoOrdemServico.criar(ordemServico);
+	}
+	
+	// LISTAR ORDEM DE SERVIÇOS
+	@GetMapping
+	public List<OrdemServico> listar(){
+		return ordemServicoRespository.findAll();
 	}
 	
 }
