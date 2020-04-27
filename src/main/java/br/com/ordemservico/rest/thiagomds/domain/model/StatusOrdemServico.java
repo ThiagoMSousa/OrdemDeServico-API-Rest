73df1ae5229f9dfbe31ee560f0e6718a8529336f
Thiago Mendes <thiagomendes_95@hotmail.com>
2020-04-26 23:51:54 -0300

    Persistencia de Dados OrdemServico

---
+++ src/main/java/br/com/ordemservico/rest/thiagomds/domain/model/StatusOrdemServico.java
@@ -0,0 +1,7 @@
+package br.com.ordemservico.rest.thiagomds.domain.model;
+
+public enum StatusOrdemServico {
+	
+	ABERTA, FINALIZADA, CANCELADA
+
+}
---
+++ src/main/java/br/com/ordemservico/rest/thiagomds/domain/service/GestaoOrdemServicoService.java
@@ -0,0 +1,37 @@
+package br.com.ordemservico.rest.thiagomds.domain.service;
+
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.ordemservico.rest.thiagomds.domain.exception.NegocioException;
+import br.com.ordemservico.rest.thiagomds.domain.model.Cliente;
+import br.com.ordemservico.rest.thiagomds.domain.model.OrdemServico;
+import br.com.ordemservico.rest.thiagomds.domain.model.StatusOrdemServico;
+import br.com.ordemservico.rest.thiagomds.domain.repository.ClienteRepository;
+import br.com.ordemservico.rest.thiagomds.domain.repository.OrdemServicoRepository;
+
+@Service
+public class GestaoOrdemServicoService {
+	
+	@Autowired
+	private OrdemServicoRepository ordemServicoRepository;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	public OrdemServico criar( OrdemServico ordemServico ) {
+		// Se encontrar o CLIENTE, salva na variavel CLIENTE
+		// Caso contrario, enviar a EXCEPTION
+		Cliente cliente = clienteRepository.findById( ordemServico.getCliente().getId() )
+				.orElseThrow( () -> new NegocioException("Cliente Não Encontrado !!!") );
+		
+		ordemServico.setCliente(cliente);
+		ordemServico.setStatus( StatusOrdemServico.ABERTA );
+		ordemServico.setDataAbertura( LocalDateTime.now() );
+		
+		return ordemServicoRepository.save( ordemServico );
+	}
+
+}
