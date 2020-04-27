47c8296a1a819c6c2d0e5feeeb108a25903f16ec
Thiago Mendes <thiagomendes_95@hotmail.com>
2020-04-27 00:22:54 -0300

    Implementação Método Buscar

--- src/main/java/br/com/ordemservico/rest/thiagomds/api/controller/OrdemServicoController.java
+++ src/main/java/br/com/ordemservico/rest/thiagomds/api/controller/OrdemServicoController.java
@@ -1,12 +1,15 @@
 package br.com.ordemservico.rest.thiagomds.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,10 +39,22 @@ public class OrdemServicoController {
 		return gestaoOrdemServico.criar(ordemServico);
 	}
 	
-	// LISTAR ORDEM DE SERVIÇOS
+	// LISTAR TODAS AS ORDENS DE SERVIÇOS
 	@GetMapping
 	public List<OrdemServico> listar(){
 		return ordemServicoRespository.findAll();
 	}
 	
+	// BUSCAR POR ORDEM DE SERVIÇO
+	@GetMapping("/{ordemServicoId}")
+	public ResponseEntity<OrdemServico> buscar( @PathVariable Long ordemServicoId ) {
+		Optional<OrdemServico> ordemServico = ordemServicoRespository.findById(ordemServicoId);
+		
+		// Se Encontar a Ordem de Serviço, Retorna OK com os Dados da Ordem Serviço
+		if( ordemServico.isPresent() ) {
+			return ResponseEntity.ok( ordemServico.get() );
+		}
+		// Caso Contrario, Retorna 404
+		return ResponseEntity.notFound().build();
+	}
 }
